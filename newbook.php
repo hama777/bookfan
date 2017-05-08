03a7a1fd4a636b5b21bad2e9802eacf89d4c98fc
hama777 <GEB01604@nifty.ne.jp>
2017-05-08 18:50:51 +0900

    new file

---
+++ newbook.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <form action="book.php" method="post">
+        総ページ数: <input type="text" name="totalpage"><br>
+        <input type="submit" value="ok">
+   
+        <input type="hidden" name="formtype" value="newbook">
+
+    </form>
+
+</body>
+</html>
