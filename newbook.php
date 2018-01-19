f24db3fb06cf17a8f251182c694ee15b549e7b7f
hama777 <GEB01604@nifty.ne.jp>
2018-01-19 17:18:54 +0900

    01/19 17:18  Version2

--- newbook.php
+++ newbook.php
@@ -1,14 +1,40 @@
+<?php
+static $filename;
+
+if ($_POST['type'] == 1  ) {
+    $filename = "bookinfo1.txt" ;
+}
+if ($_POST['type'] == 2  ) {
+    $filename = "bookinfo2.txt" ;
+}
+if ($_POST['type'] == 3  ) {
+    $filename = "bookinfo3.txt" ;
+}
+
+echo $_POST['type'] ; 
+if ($_SERVER['REQUEST_METHOD'] == 'POST' ) {
+    echo "Open" ;
+        $fp = fopen($filename ,"w");
+        fwrite($fp, $_POST['totalpage']."\n" );
+        fwrite($fp, $_POST['bookname']."\n" );
+        fclose($fp);
+        if ($_POST['totalpage'] != ""  ) {
+            echo '<meta http-equiv="refresh" content="0;URL=book.php">' ;
+        }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="utf-8">
 </head>
 <body>
-    <form action="book.php" method="post">
+    <form action="" method="post">
         書名 : <input type="text" name="bookname"><br>
         ページ数: <input type="text" name="totalpage"><br>
+          <input type="hidden" name="type" value=<?php echo $_POST['type']; ?>>
         <input type="submit" value="ok">
-        <input type="hidden" name="formtype" value="newbook">
     </form>
 </body>
 </html>
