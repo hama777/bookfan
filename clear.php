78f0a4518e5f4b384134010151a567606ebbfedb
hama777 <GEB01604@nifty.ne.jp>
2018-02-25 12:34:01 +0900

    02/25 12:33 本の情報もクリアする。

--- clear.php
+++ clear.php
@@ -3,16 +3,22 @@ static $filename;
 
 if ($_POST['type'] == 1  ) {
     $datafile = "book.txt" ;
+    $infofile = "bookinfo1.txt" ;
 }
 if ($_POST['type'] == 2  ) {
     $datafile = "book2.txt" ;
+    $infofile = "bookinfo2.txt" ;
 }
 if ($_POST['type'] == 3  ) {
     $datafile = "book3.txt" ;
+    $infofile = "bookinfo3.txt" ;
 }
 
 $fp = fopen($datafile ,"w");
 fclose($fp);
+$fp = fopen($infofile ,"w");
+fclose($fp);
+
 echo '<meta http-equiv="refresh" content="0;URL=book.php">' ;
 
 ?>
